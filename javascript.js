0820561c6348fe35d4ee9f9e033118f133041a44
Diana Diakite <dianadiakite05@gmail.com>
2018-11-10 20:22:43 +0000

    final changes2

--- javascript.js
+++ javascript.js
@@ -2,15 +2,15 @@ var slideindex = 0;
 showSlides();
 
 function showSlides() {
-    
+    console.log('heree')
     var slides = document.getElementsByClassName("myimages");
     for (var i = 0; i < slides.length; i++) {
-        slides[i].stlye.display = "none";
+        slides[i].style.display = "none";
     }
     slideindex++;
     if (slideindex >= slides.length) {
         slideindex = 0
     }
-    slides[slideindex].stlye.display = "block";
+    slides[slideindex].style.display = "block";
     setTimeout(showSlides, 4000);
 }
